f549e8b0410f40924a6f02feea938dca6adcd4be
lakshitha <lakshithajayaweera@gmail.com>
2016-06-15 23:02:57 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,27 @@
+var app =angular.module('mainApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/',{
+		templateUrl :'login.html'
+	})
+	.when('/dashboard',{
+		templateUrl:'dashboard.html'
+	})
+	.otherwise({
+	    redirectTo: '/'
+	});
+	
+});
+
+app.controller('loginCtrl', function($scope,$location){
+	$scope.submit = function(){
+		var name = $scope.username;
+		var password = $scope.password;
+
+		if($scope.username == 'admin' && $scope.password == 'admin'){
+			$location.path('/dashboard');
+		}
+	};
+	
+});
